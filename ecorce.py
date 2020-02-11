5f2ecb2baa2a2d4221a3f5f892ea265a87cdf87c
ecorce <chloe.tommasi@free.fr>
2020-02-11 17:54:35 +0100

    basic app

---
+++ ecorce.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route('/<path:path>')
+def send_file(path):
+    return app.send_static_file(path)
+
+@app.route("/")
+def hello2():
+    return "<h1 style='color:blue'>Welcome to ECORCE</h1>"
+
+@app.route("/test")
+def user():
+	conn = psycopg2.connect(host="localhost",database="ecorce", user="postgres", password="geonum2020")
+	cursor = conn.cursor()
+	cursor.execute("""
+            select json_build_object(
+                'type', 'FeatureCollection',
+                'features', json_agg(ST_AsGeoJSON(utilisateur.*)::json)
+            ) as geojson
+            from utilisateur
+            """)
+	user = cursor.fetchone()
+	return str(user)
+
