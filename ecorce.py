fd9972c90ce0d0095f3e0d8551b675d334aad306
CTomm <44056621+CTomm@users.noreply.github.com>
2020-02-18 09:19:31 +0100

    Add files via upload

--- ecorce.py
+++ ecorce.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, session
+#import Session
 import psycopg2
 
 app = Flask(__name__)
+Session(app)
+app.secret_key = "ecorce2020"
 
 @app.route('/<path:path>')
 def send_file(path):
@@ -13,32 +16,37 @@ def hello2():
 
 @app.route("/test")
 def user():
-	conn = psycopg2.connect(host="localhost",database="ecorce", user="postgres", password="geonum2020")
-	cursor = conn.cursor()
-	cursor.execute("""
+    conn = psycopg2.connect(host="localhost",database="ecorce", user="postgres", password="postgres")
+    cursor = conn.cursor()
+    cursor.execute("""
             select json_build_object(
                 'type', 'FeatureCollection',
-                'features', json_agg(ST_AsGeoJSON(user84.*)::json)
+                'features', json_agg(ST_AsGeoJSON(utilisateur.*)::json)
             ) as geojson
-            from user84
+            from utilisateur
             """)
-	return cursor.fetchone()[0]
+    test = cursor.fetchone()[0]
+    return jsonify(test)
 
+@app.route('/myvalue')
+def test():
+    my_var = session.get('emissions', None)
+    return str(my_var)
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
-    poulet = request.form['Q3_poulet']
+    #poulet = request.form['Q3_poulet']
     #Récupérer toutes les valeurs du questionnaire
     #Calculer les émissions totales
     #Envoyer le résultat à psql
-    # conn = psycopg2.connect(host="localhost",database="ecorce", user="postgres", password="geonum2020")
+    # conn = psycopg2.connect(host="localhost",database="ecorce", user="postgres", password="postgres")
     # cursor = conn.cursor()
     # cursor.execute("""
     #         update utilisateur set conso = 4000
-    #         where id = """ + poulet
+    #         where id = 3"""
     #         )
     # conn.commit()
-    #conn.close()
+    # conn.close()
     #return render_template("wait.html")
 
 
@@ -48,13 +56,13 @@ def handle_data():
     alimfixe = 282.72608
 
     #VARIABLE  
-    poulet = 28.34551491*int(request.form['Q3_poulet'])
-    porc = 34.90253149*int(request.form['Q3_porc'])
-    agneau = 334.2929584*int(request.form['Q3_agneau'])
-    boeuf = 235.1966514*int(request.form['Q3_boeuf'])
-    poisson = 22.78607013*int(request.form['Q4'])
+    poulet = 28.34551491*float(request.form['Q3_poulet'])
+    porc = 34.90253149*float(request.form['Q3_porc'])
+    agneau = 334.2929584*float(request.form['Q3_agneau'])
+    boeuf = 235.1966514*float(request.form['Q3_boeuf'])
+    poisson = 22.78607013*float(request.form['Q4'])
     oeufs = 11.10564*int(request.form['Q5'])
-    fromage = 356.72*int(request.form['Q6'])
+    fromage = 356.72*float(request.form['Q6'])
     lait = 52.53948708*int(request.form['Q7'])
 
     #Légumes de saison ou pas
@@ -65,15 +73,19 @@ def handle_data():
 
     #ENERGIE
     if request.form['Q8'] == "A":
-        elect = 696*int(request.form['Q9'])
+        if request.form['Q9_electrique'] != None:
+            elect = 696*int(request.form['Q9_electrique'])
+        if request.form['Q9_gaz'] != None:
+            gaz = 230*int(request.form['Q9_gaz'])
+        if request.form['Q9_fioul'] != None:
+            fioul = 275*int(request.form['Q9_fioul'])
+        if request.form['Q9_bois'] != None:
+            bois = 13*int(request.form['Q9_bois'])
     else:
         elect = 0
-
-
-    elect = 696 #VAR
-    fioul = 275 #VAR
-    bois = 13 #VAR
-    gaz = 230  #VAR
+        gaz = 0
+        fioul = 0
+        bois = 0
 
     #TRANSPORTS
     
@@ -84,10 +96,11 @@ def handle_data():
     car = 58.5*int(request.form['Q16'])
     avion = 144.6*int(request.form['Q17'])
 
-    emissions = alimfixe+poulet+porc+agneau+boeuf+poisson+oeufs+fromage+lait+leg+ elect+fioul+bois+gaz+ tc_s+voit_s+train+voit_annee+car+avion
-    return str(emissions)
+    emissions = alimfixe+poulet+porc+agneau+boeuf+poisson+oeufs+fromage+lait+leg+elect+fioul+bois+gaz+ tc_s+voit_s+train+voit_annee+car+avion
+    session['emissions'] = emissions
+    return render_template("wait.html")
 
 
 
-
+#A enlever quand on va sur la VM
 app.run(host='0.0.0.0', port='5000')
